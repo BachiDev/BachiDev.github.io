14b5f25d935ecb083ea4e3309dc22a2e5de11d69
Fabian <bachmayer.fabian@gmail.com>
2025-08-01 21:03:26 +0200

    impressum

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,19 @@
 export function Footer() {
   return (
-    <footer className="flex flex-col gap-4 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t border-neutral-800">
-      <p className="text-xs text-neutral-400">© 2025 Fabian Bachmayer. All rights reserved.</p>
-      
+    <footer className="flex flex-col gap-4 sm:flex-row py-6 w-full shrink-0 items-start px-4 md:px-6 border-t border-neutral-800">
+      <div id="imprint" className="text-xs text-neutral-400 text-center sm:text-left">
+        <p>Fabian Bachmayer</p>
+        <p>Bernoullistraße 4/4/20</p>
+        <p>1220 Vienna, Austria</p>
+        <br/>
+        <p>Email: bachiaidev@gmail.com</p>
+        <br/>
+        <p>Services in automatic data processing and information technology</p>
+        <p>UID: </p>
+        <p>Court of commercial registration: Commercial Court of Vienna</p>
+        <p>Member of the Austrian Economic Chamber</p>
+      </div>
+      <p className="text-xs text-neutral-400 sm:ml-auto">© 2025 Fabian Bachmayer. All rights reserved.</p>
     </footer>
   );
 }
